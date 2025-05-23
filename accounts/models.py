66dc0337576408bb70d444683cc9a959692c114f
usxc <tinypopopo@gmail.com>
2025-05-23 13:49:28 +0900

    構築(accounts): models.pyに、CustomUserを構築

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    student_id = models.CharField(primary_key=True, max_length=8, verbose_name="学籍番号") # 必須入力
+    first_name = models.CharField(max_length=60, blank=False, null=False, verbose_name="名") # 必須入力
+    last_name = models.CharField(max_length=60, blank=False, null=False, verbose_name="姓") # 必須入力
+    email = models.EmailField(unique=True, blank=True, null=True, verbose_name="メールアドレス") # 任意入力
+    username = models.CharField(max_length=60, unique=True, blank=True, null=True, verbose_name="ユーザー ネーム") # 任意入力
+    
+    USERNAME_FIELD = student_id
+
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'email']
+
+    class Meta:
+        ordering = ['student_id']
+
+    def __str__(self):
+        display_name = f"{self.last_name}{self.first_name}".strip()
+        return f"{self.student_id}_{display_name}"
+    
+    def get_full_name(self):
+        full_name = f"{self.last_name}{self.first_name}".strip()
+        return full_name
+    
+    def get_short_name(self):
+        return self.first_name
