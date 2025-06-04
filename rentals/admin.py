65a010c020f1331697dad1658fcdba42cb3c5011
usxc <tinypopopo@gmail.com>
2025-06-04 15:52:58 +0900

    構築(rentals): 管理画面でのBookモデル、Rentalモデル登録

--- rentals/admin.py
+++ rentals/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Book, Rental
 
-# Register your models here.
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'isbn', 'created_at', 'updated_at')
+    search_fields = ('title', 'author', 'isbn')
+    list_filter = ('author', 'created_at')
+    readonly_fields = ('id', 'created_at', 'updated_at')
+
+@admin.register(Rental)
+class RentalAdmin(admin.ModelAdmin):
+    list_display = ('book', 'user', 'rental_date', 'return_due_date', 'is_returned', 'returned_at')
+    search_fields = ('book__title', 'user__username', 'user__student_id')
+    list_filter = ('is_returned', 'user', 'book', 'rental_date')
+    autocomplete_fields = ['book', 'user']
+    readonly_fields = ('id', 'created_at', 'updated_at', 'rental_date', 'returned_at')
