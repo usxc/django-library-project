aee5dc3301c7f6ccffdd09240f588a0da8363193
usxc <tinypopopo@gmail.com>
2025-06-04 15:50:05 +0900

    構築(accounts): 管理画面用のフォームを作成

---
+++ accounts/forms.py
@@ -0,0 +1,12 @@
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from .models import CustomUser
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = CustomUser
+        fields = UserCreationForm.Meta.fields + ('student_id', 'email', 'first_name', 'last_name')
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta(UserChangeForm.Meta):
+        model = CustomUser
+        fields = UserChangeForm.Meta.fields
